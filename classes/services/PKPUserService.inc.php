11fa49a36e72948e86c2d736eac790ccefacf593
Andrii Sememets <semteacher@gmail.com>
2020-07-21 11:00:34 +0300

    re-implementation of TNMU feature: - allow subeditor to access unassigned submissions
     Conflicts:
    	classes/services/PKPUserService.inc.php
    resolved

--- classes/services/PKPUserService.inc.php
+++ classes/services/PKPUserService.inc.php
@@ -552,7 +552,7 @@ class PKPUserService implements EntityPropertyInterface, EntityReadInterface {
 			$roleDao = DAORegistry::getDAO('RoleDAO'); /* @var $roleDao RoleDAO */
 			$userRoles = $roleDao->getByUserId($userId, $contextId);
 			foreach ($userRoles as $userRole) {
-				if (in_array($userRole->getId(), [ROLE_ID_SITE_ADMIN, ROLE_ID_MANAGER])) {
+				if (in_array($userRole->getId(), [ROLE_ID_SUB_EDITOR, ROLE_ID_SITE_ADMIN, ROLE_ID_MANAGER])) {
 					$accessibleStageRoles[] = $userRole->getId();
 				}
 			}
